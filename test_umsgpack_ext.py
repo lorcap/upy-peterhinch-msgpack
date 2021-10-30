16040a5ad1741fc39492b7df69baf7b7c16000d7
Lorenzo Cappelletti <lorenzo.cappelletti@gmail.com>
2021-10-30 06:43:44 +0200

    Add support to Pin

--- test_umsgpack_ext.py
+++ test_umsgpack_ext.py
@@ -5,13 +5,14 @@
 #   $ micropython test_umsgpack_ext.py
 
 import unittest
+from machine import Pin
 
 import umsgpack
-from umsgpack.umsgpack_ext import Complex, Set, Tuple
+from umsgpack.umsgpack_ext import Complex, Set, Tuple, PinInt, PinStr, PinTuple
 
 
-def roundtrip (obj):
-    return umsgpack.loads(umsgpack.dumps(obj))
+def roundtrip (obj, **options):
+    return umsgpack.loads(umsgpack.dumps(obj, **options), **options)
 
 class TestUmsgpackExt(unittest.TestCase):
 
@@ -33,6 +34,26 @@ class TestUmsgpackExt(unittest.TestCase):
         self.assertEqual(str(ext), "Tuple((1, 2, 3))")
         self.assertEqual(obj, roundtrip(obj))
 
+    def test_pin_int(self):
+        Pin.TYPE=1
+        obj = Pin(0)
+        ext = PinInt(obj)
+        self.assertEqual(str(ext), "Pin(0)")
+        self.assertEqual(obj, roundtrip(obj, pin_id=Pin.TYPE))
+
+    def test_pin_str(self):
+        Pin.TYPE=2
+        obj = Pin("LED1")
+        ext = PinStr(obj)
+        self.assertEqual(str(ext), "Pin('LED1')")
+        self.assertEqual(obj, roundtrip(obj, pin_id=Pin.TYPE))
+
+    def test_pin_tuple(self):
+        Pin.TYPE=3
+        obj = Pin(("GPIO_1", 1))
+        ext = PinTuple(obj)
+        self.assertEqual(str(ext), "Pin(('GPIO_1', 1))")
+        self.assertEqual(obj, roundtrip(obj, pin_id=Pin.TYPE))
 
 if __name__ == '__main__':
     unittest.main()
--- umsgpack/umsgpack_ext.py
+++ umsgpack/umsgpack_ext.py
@@ -12,9 +12,14 @@
 import umsgpack
 import struct
 
+try:
+    from machine import Pin
+except ImportError:
+    pass
+
 # Entries in mpext are required where types are to be handled without declaring
 # an ext_serializable class in the application. This example enables complex,
-# tuple and set types to be packed as if they were native to umsgpack.
+# tuple, set and Pin types to be packed as if they were native to umsgpack.
 # Options (kwargs to dump and dumps) may be passed to constructor including new
 # type-specific options
 def mpext(obj, options):
@@ -24,6 +29,16 @@ def mpext(obj, options):
         return Set(obj)
     if isinstance(obj, tuple):
         return Tuple(obj)
+    if 'pin_id' in options and isinstance(obj, Pin):
+        pin_id = options['pin_id']
+        if pin_id == 1:
+            return PinInt(obj)
+        if pin_id == 2:
+            return PinStr(obj)
+        if pin_id == 3:
+            return PinTuple(obj)
+        else:
+            raise umsgpack.UnsupportedTypeException
     return obj
 
 @umsgpack.ext_serializable(0x50)
@@ -70,3 +85,52 @@ class Tuple:
     @staticmethod
     def unpackb(data, options):
         return tuple(umsgpack.loads(data))
+
+@umsgpack.ext_serializable(0x53)
+class PinInt:
+    def __init__(self, p):
+        self.pin = p.pin()
+
+    def __str__(self):
+        return "Pin({})".format(self.pin)
+
+    def packb(self):
+        return self.pin.to_bytes(1, "big")
+
+    @staticmethod
+    def unpackb(data, options):
+        return Pin(int.from_bytes(data, "big"))
+
+@umsgpack.ext_serializable(0x54)
+class PinStr:
+    def __init__(self, p):
+        self.name = p.name()
+
+    def __str__(self):
+        return "Pin('{}')".format(self.name)
+
+    def packb(self):
+        return self.name.encode()
+
+    @staticmethod
+    def unpackb(data, options):
+        return Pin(data.decode())
+
+@umsgpack.ext_serializable(0x55)
+class PinTuple:
+    def __init__(self, p):
+        self.port = p.port()
+        self.pin = p.pin()
+
+    def __str__(self):
+        return "Pin(('{}', {}))".format(self.port, self.pin)
+
+    def packb(self):
+        lp = len(self.port)
+        return struct.pack("B{}s".format(lp), self.pin, self.port.encode())
+
+    @staticmethod
+    def unpackb(data, options):
+        lp = len(data) - 1
+        pin, port = struct.unpack("B{}s".format(lp), data)
+        return Pin((port.decode(), pin))
